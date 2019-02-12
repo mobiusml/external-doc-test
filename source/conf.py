afa749690d5f87646a1d3f53d8b712c092223ec5
Isabel Schwende <isabel@mobius.ml>
2019-02-12 11:30:23 +0100

    Add favicon and sidebar image

--- source/conf.py
+++ source/conf.py
@@ -115,6 +115,14 @@ html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'sdk_external_docdoc'
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "data/manifest_icon.png"
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = "data/manifest_icon.png"
 
 # -- Options for LaTeX output ------------------------------------------------
 
